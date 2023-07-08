3846f3da45e444c1d7b25efda26ed44dba9a8d86
Web Developer Mamun <134207938+webdevelopermamun-official@users.noreply.github.com>
2023-07-08 10:19:55 +0600

    Add files via upload

---
+++ assets/js/data.js
@@ -0,0 +1,145 @@
+const facebookPost = [
+    {
+        id: 1,
+        post_content: `হুট করে 👽`,
+        post_media: "https://scontent.fdac41-1.fna.fbcdn.net/v/t39.30808-6/358124732_10224593205357364_3723954789401690685_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_ohc=3Ls5i19Y9k0AX8t43-5&_nc_ht=scontent.fdac41-1.fna&oh=00_AfCZ9ZjH16D-z2KrGaHdSFOIJmq4MTr-nO9xrLxXSUd7xw&oe=64A9EB09",
+        post_like: 270,
+        post_comment: 76,
+        post_ago: '3h',
+        post_author: {
+            name:"আশরাফুল হক",
+            photo: "https://scontent.fdac41-1.fna.fbcdn.net/v/t39.30808-6/333936078_1361956414655948_834213367116914942_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=cX9DNt1Gk6UAX8qHa2W&_nc_ht=scontent.fdac41-1.fna&oh=00_AfC_mt3GX4cFFhBkwUQqxexHO-sqc1wjWbMRR3K2yOX30g&oe=64A91DD1"
+        }
+    },
+    {
+        id: 2,
+        post_content: `Ariful Islam Noyon ভাইয়া আমাদের জন্য ফ্রিতে অনেক কষ্ট করে Crocoblock নিয়ে ভিডিও তৈরি করতেছে । আমরা Crocoblock নিয়ে সবকিছু শিখতে চাই । তারমধ্যে অন্যতম profile Builder নিয়ে একটা লাইভ ক্লাস হলে ভালোই হয় । Ariful Islam Noyon ভাইয়ার দৃষ্টি আকষর্ণ করে বলতেছি  Crocoblock  এর একটা লাইভ ক্লাস চাই  চাই  চাই ।।।
+        #freelancing #wordpress #crocodile #jetengine_profile_builder #freelancing_premium_support #crocoblock_help_bangladesh #crocoblock_expert`,
+        post_media: "https://scontent.fdac41-1.fna.fbcdn.net/v/t39.30808-6/357704523_1302186957365901_1192265548188903729_n.jpg?stp=dst-jpg_p180x540&_nc_cat=102&ccb=1-7&_nc_sid=5cd70e&_nc_ohc=muxNCNekob8AX92Rqzq&_nc_ht=scontent.fdac41-1.fna&oh=00_AfB9tPbPnXHScGHHcxI2xDJKAuaZsEddsmZyBkXCDEabSQ&oe=64A930E9",
+        post_like: 5,
+        post_comment: 2,
+        post_ago: '3 minutes ago',
+        post_author: {
+            name:"Nayma Aktar Tisha",
+            photo: "https://scontent.fdac41-1.fna.fbcdn.net/v/t39.30808-6/261013364_927802501471017_8757513175175795322_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=7Zl6yHmIihMAX-UX2jc&_nc_ht=scontent.fdac41-1.fna&oh=00_AfBkwCGcBCUHAbffMmDtysoQfEYwOEwEjHckd4ignnGLvQ&oe=64A8B500"
+        }
+    },
+    {
+        id: 3,
+        post_content: `আলহামদুলিল্লাহ ক্ষুদ্র প্রচেষ্টায় ২ দিনে এজেন্সি ওয়েবসাইট তৈরী করলাম ।
+        কেমন হলো কমেন্ট বক্সে জানাবেন।
+        ধন্যবাদ।
+        — feeling excited in Jashore City.`,
+        post_media: "https://scontent.fdac41-1.fna.fbcdn.net/v/t39.30808-6/346642362_971253370710876_1645527934782274402_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=5cd70e&_nc_ohc=f3ogg4OvvloAX_T224H&_nc_oc=AQnGPn1o-84A1xxG4yrDsxmlqdvaeunF04hEeD68qg__N_dJ0_1Uos3gLBItWHx-Iwo&_nc_ht=scontent.fdac41-1.fna&oh=00_AfA2SVAOvnntP2B33WbmA8b4f8SrBPIUPu0n1nJ-_XHAww&oe=64A96E04",
+        post_like: 76,
+        post_comment: 17,
+        post_ago: '10 hours ago',
+        post_author: {
+            name:"Md Jubair Ahmed",
+            photo: "https://scontent.fdac41-1.fna.fbcdn.net/v/t39.30808-6/357100262_1593083824434954_1365653955690731279_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=KDHkO37UHUIAX8z1oAg&_nc_ht=scontent.fdac41-1.fna&oh=00_AfAvLcoHw3BwYe9GvRrdqxi2IlqFmW5JESlpw-YdO1eMoA&oe=64A8B190"
+        }
+    },
+    {
+        id: 4,
+        post_content: `যাক আমি আমাকে শান্তনা দিতে পারবো🙈`,
+        post_media: "https://scontent.fdac41-1.fna.fbcdn.net/v/t39.30808-6/357122746_1334926893761358_4882346579532178757_n.jpg?stp=dst-jpg_p526x296&_nc_cat=1&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=E9zh4Dpv8ZAAX85tqhj&_nc_ht=scontent.fdac41-1.fna&oh=00_AfDrDaB6nymdS7iWXO5jvpNL2tWLGOGfEF2Koy8mKu2-GA&oe=64A9DC77",
+        post_like: '1k',
+        post_comment: 897,
+        post_ago: '6 Days ago',
+        post_author: {
+            name:"Shahariar Islam Rahel",
+            photo: "https://scontent.fdac41-1.fna.fbcdn.net/v/t39.30808-6/356380487_1334286187158762_9012964795926718578_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=H9OIESmqBxsAX-Wztku&_nc_ht=scontent.fdac41-1.fna&oh=00_AfB8sy1fcgg3ULi7Rrge4M3iMFiRJWtyDkeKbE2wYGYPLg&oe=64A9F85D"
+        }
+    },
+    {
+        id: 5,
+        post_content: `ভাই ভোট টা নৌকায় দিয়েন`,
+        post_media: "https://scontent.fdac41-1.fna.fbcdn.net/v/t39.30808-6/358068812_1320270752241281_5307448984636785955_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=o016uqKHBDwAX8QRpC_&_nc_ht=scontent.fdac41-1.fna&oh=00_AfDcYefrLpPJmjQ8bepg8xwIyx0WASnAlB0rwWbdPVI6dg&oe=64AA05A9",
+        post_like: 24,
+        post_comment: 1,
+        post_ago: 'a Days ago',
+        post_author: {
+            name:"Mohammad Raju",
+            photo: "https://scontent.fdac41-1.fna.fbcdn.net/v/t39.30808-6/316538373_1183223152612709_1046600466526767434_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=-Z1E2dG1q6IAX9AUSTB&_nc_ht=scontent.fdac41-1.fna&oh=00_AfCDw9J7pTYHR-Hm1X0isJKh2jvbqjtP0ejFOwTnBKLrWg&oe=64A96D62"
+        }
+    },
+    {
+        id: 6,
+        post_content: `👉যারা বউ রাইখা বিদেশ যায়, তারা হইলো ভোদাই 😜
+        👉ভালো হয়ে যা মাসুদ 😄
+        👉মাসুদ ভালো হইলি না।🙄 `,
+        post_media: "https://scontent.fdac41-1.fna.fbcdn.net/v/t39.30808-6/357404096_3434403290155946_2106804655492730299_n.jpg?stp=dst-jpg_s600x600&_nc_cat=111&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=zYR6mK19AJEAX-Uzn1K&_nc_ht=scontent.fdac41-1.fna&oh=00_AfByAtJ5T27eB-ky53BNMdghSZ7uyiqil9C1lkKAhbpQHQ&oe=64A9B831",
+        post_like: 7,
+        post_comment: 2,
+        post_ago: '10m',
+        post_author: {
+            name:"Rafikul Islam Rafi",
+            photo: "https://scontent.fdac41-1.fna.fbcdn.net/v/t39.30808-6/341310781_894633614959924_4307616928611813712_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=MLLJIZE8oHQAX-_xi2c&_nc_ht=scontent.fdac41-1.fna&oh=00_AfDc79qj4enIEfPP0az6CSOboVZvOR7pzq7vxCN9dYDsOQ&oe=64A87252"
+        }
+    },
+    {
+        id: 7,
+        post_content: `Happy Level Up Day 🎈🎂( বন্ধু )😍
+        Many many happy returns of the day.🖤🌸
+        দোয়া করি আল্লাহ তায়ালা তোর মনের সকল নেক ইচ্ছেগুলো পূরণ করুন আমিন।
+        দোয়া করি আল্লাহ তায়ালা তোকে সবসময় সুস্থ রাখুক 
+        ,আনন্দে রাখুক 🥰🥰`,
+        post_media: "https://scontent.fdac41-1.fna.fbcdn.net/v/t39.30808-6/357537223_208417532164246_1916848098322661579_n.jpg?stp=dst-jpg_s600x600&_nc_cat=108&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=MY3YXZT942wAX94W3w5&_nc_ht=scontent.fdac41-1.fna&oh=00_AfCpOiGz0ysuS2UFqOOog1UvgnKlQVD2Q29JAWAmuwXJTw&oe=64A95413",
+        post_like: 33,
+        post_comment: 25,
+        post_ago: '2 days ago',
+        post_author: {
+            name:"N A Salman (Nahian)",
+            photo: "https://scontent.fdac41-1.fna.fbcdn.net/v/t39.30808-6/357517010_209509715388361_7099127907000106688_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=R9HNnAb8K2oAX_ZKKGw&_nc_ht=scontent.fdac41-1.fna&oh=00_AfCYXHw-aXMjoDdwG73ZRhq_4BhUtIJ7HrEuPe76OS5Rmg&oe=64A8DFDE"
+        }
+    },
+    {
+        id: 8,
+        post_content: `𝐎𝐍𝐋𝐈𝐍𝐄 𝐖𝐎𝐑𝐊 𝐅𝐑𝐎𝐌 𝐇𝐎𝐌𝐄
+        𝐓𝐈𝐌𝐄.                   : 1-5 𝐡𝐫𝐬 (Flexible hrs)
+        𝐈𝐍𝐂𝐎𝐌𝐄              : $297.00 USD (daily)
+        𝐀𝐆𝐄                      : any age
+        𝐄𝐃𝐔𝐂𝐀𝐓𝐈𝐎𝐍      :no 
+        𝐖𝐎𝐑𝐊                 : 𝐰𝐨𝐫𝐤 𝐟𝐫𝐨𝐦 𝐡𝐨𝐦e
+        𝐑𝐄𝐐𝐔𝐈𝐑𝐄𝐌𝐄𝐍𝐓𝐒 : 𝐬𝐦𝐚𝐫𝐭𝐩𝐡𝐨𝐧𝐞 
+        𝗦𝘁𝗮𝗳𝗳, 𝗙𝗿𝗲𝘀𝗵𝗲𝗿, 𝘀𝘁𝘂𝗱𝗲𝗻𝘁𝘀, 𝗵𝗼𝘂𝘀𝗲𝘄𝗶𝘃𝗲𝘀,  𝗮𝗻𝘆𝗼𝗻𝗲 𝗰𝗮𝗻 𝘄𝗼𝗿𝗸
+        𝗜𝗳 𝘆𝗼𝘂 𝗮𝗿𝗲 𝗶𝗻𝘁𝗲𝗿𝗲𝘀𝘁𝗲𝗱 𝘀𝗮𝘆 𝗱𝗶𝗿𝗲𝗰𝘁 "𝗬𝗘𝗦"
+        Details will be sent Inbox.`,
+        post_media: "https://scontent.fdac41-1.fna.fbcdn.net/v/t39.30808-6/358058759_1439502753481204_3317826528184525051_n.jpg?stp=dst-jpg_p526x296&_nc_cat=108&ccb=1-7&_nc_sid=5cd70e&_nc_ohc=r6gxcv9k_YEAX-4ozRX&_nc_ht=scontent.fdac41-1.fna&oh=00_AfArpqhjBM5DAN7fYZuCdpl0clGGbC-LkYNMARN6KQ8wrA&oe=64A9FF18",
+        post_like: 14,
+        post_comment: 69,
+        post_ago: '10 hours ago',
+        post_author: {
+            name:"Idris Horlaitan Adeniyi",
+            photo: "https://scontent.fdac41-1.fna.fbcdn.net/v/t39.30808-6/341794061_2177042242502174_5231390466520204074_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=rl1oMtdUAjgAX8Yrejo&_nc_ht=scontent.fdac41-1.fna&oh=00_AfB7jv5PBirl6VD5Am9qxAXZ0ymPPfS75zq20XkzE6J2eQ&oe=64A9C68F"
+        }
+    },
+    {
+        id: 9,
+        post_content: `জীবন বইয়ের পাতার মতো যতই উল্টাবে ততই শিখবে 😎        `,
+        post_media: "https://scontent.fdac41-1.fna.fbcdn.net/v/t39.30808-6/344765660_966667971313799_6644345011102041809_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=e3f864&_nc_ohc=q7Lm_VKrAQQAX_nVrnw&_nc_ht=scontent.fdac41-1.fna&oh=00_AfDToq3DS13Qk1gaNPRr6yJt_o9KglyD-tvcq6gZd8sLGQ&oe=64A8E881",
+        post_like: 128,
+        post_comment: 104,
+        post_ago: '3 days ago',
+        post_author: {
+            name:"N A Salman",
+            photo: "https://scontent.fdac41-1.fna.fbcdn.net/v/t39.30808-6/357517010_209509715388361_7099127907000106688_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=R9HNnAb8K2oAX_ZKKGw&_nc_ht=scontent.fdac41-1.fna&oh=00_AfCYXHw-aXMjoDdwG73ZRhq_4BhUtIJ7HrEuPe76OS5Rmg&oe=64A8DFDE"
+        }
+    },
+    {
+        id: 10,
+        post_content: `Prepare for IELTS with the British Council’s easily accessible and free study resources.        `,
+        post_media: "https://scontent.fdac41-1.fna.fbcdn.net/v/t45.1600-4/330365680_23853055416120382_3337460840591118626_n.png?stp=cp0_dst-jpg_p526x296_q90_spS444&_nc_cat=1&ccb=1-7&_nc_sid=e84a38&_nc_ohc=JIsBnYmVjQ4AX8sq1Lg&_nc_ht=scontent.fdac41-1.fna&oh=00_AfBrUtrQGMXR_9tI5TZhAVtGHERf4wwvjcNW25yllFbD4w&oe=64A93D09",
+        post_like: 316,
+        post_comment: 6,
+        post_ago: 'Sponsored',
+        post_author: {
+            name:"British Council Bangladesh",
+            photo: "https://scontent.fdac41-1.fna.fbcdn.net/v/t39.30808-6/276989587_339532691550746_5568240684775942197_n.png?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=9poTCPMBxtcAX9A-WKv&_nc_ht=scontent.fdac41-1.fna&oh=00_AfBCuAy7tIBGPU5lOcj4Un50Dg_Qivnfk8lPGlE6ZTOnVA&oe=64A8E604"
+        }
+    },
+
+
+] 
+
+  
